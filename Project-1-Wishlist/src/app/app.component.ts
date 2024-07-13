1e5d20eb975e587f6f4f09ddfb8c7d7b8831874b
deveshjoshi101 <deveshjoshi101@gmail.com>
2024-07-13 23:45:13 +0530

    added getter for fulfilled items

--- Project-1-Wishlist/src/app/app.component.ts
+++ Project-1-Wishlist/src/app/app.component.ts
@@ -13,46 +13,42 @@ import { FormsModule } from '@angular/forms';
 })
 export class AppComponent {
   items: wishItem[] = [
-    new wishItem('$100,000,000,000,000', true),
-    new wishItem('Learn Angular', true),
-    new wishItem('Build an app', true),
-    new wishItem('Share my knowledge', true),
-    new wishItem('Try out new hobbies', true),
-    new wishItem('Become a freelancer', true),
-    new wishItem('Eat healthy', true),
-    new wishItem('Have fun', true),
-    new wishItem('Take care of myself', true),
-    new wishItem('Travel', true),
-    new wishItem('Be kind to others', true),
-    new wishItem('Surround yourself with people who make you happy', true),
-    new wishItem('Learn about history', true),
-    new wishItem('Get certified in a new skill', true),
-    new wishItem('Practice mindfulness', true),
-    new wishItem('Start meditation', true),
-    new wishItem('Be a better at connecting with others', true),
-    new wishItem('Be a better cook', true),
-    new wishItem('Be a better at baking', true),
-    new wishItem('Eat Junk food'),
+    // new wishItem('$100,000,000,000,000', true),
+    // new wishItem('Learn Angular', true),
+    // new wishItem('Build an app', true),
+    // new wishItem('Share my knowledge', true),
+    // new wishItem('Try out new hobbies', true),
   ];
 
   title = 'my wishlist';
   newWishText = '';
   listFilter: string = '';
-  visibleItems: wishItem[] = this.items;
 
   addNewWish() {
     this.items.push(new wishItem(this.newWishText));
   }
 
-  filterChanged(value: any) {
+  get visibleItems(): wishItem[] {
+    let value = this.listFilter;
+
     if (value === '0') {
-      this.visibleItems = this.items;
+      return this.items;
     } else if (value === '1') {
-      this.visibleItems = this.items.filter((item) => item.isComplete);
+      return this.items.filter((item) => item.isComplete);
     } else {
-      this.visibleItems = this.items.filter((item) => !item.isComplete);
+      return this.items.filter((item) => !item.isComplete);
     }
   }
+
+  // filterChanged(value: any) {
+  //   if (value === '0') {
+  //     this.visibleItems = this.items;
+  //   } else if (value === '1') {
+  //     this.visibleItems = this.items.filter((item) => item.isComplete);
+  //   } else {
+  //     this.visibleItems = this.items.filter((item) => !item.isComplete);
+  //   }
+  // }
 
   toggleItem(item: wishItem) {
     item.isComplete = !item.isComplete;
